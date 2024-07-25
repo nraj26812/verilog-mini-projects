efe8595bdc4a289a1a39e02c65bfa25c72f952ed
Naveen Raj <158245437+nraj26812@users.noreply.github.com>
2024-07-25 22:05:52 +0530

    Update fa.v

--- fa.v
+++ fa.v
@@ -1,10 +1,10 @@
-module fa(a,b,c,s,cout);
-input a,b,c;
-output s,cout;
+module fa(a,b,cin,s,c);
+input a,b,cin;
+output s,c;
 wire s1,s2,s3;
 xor(s1,a,b);
-xor(s,s1,c);
+xor(s,s1,cin);
 and(s2,a,b);
-and(s3,s,c);
-or(cout,s2,s3);
+and(s3,s1,cin);
+or(c,s2,s3);
 endmodule
